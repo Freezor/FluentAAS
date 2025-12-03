8c1fe96f0d4d7158bfcd1fe2e27c7abb0f2266f7
Oliver <info@oliver-fries.de>
2025-12-03 22:21:27 +0100

    FEAT Clean up code generation

--- FluentAAS/FluentAAS.Core/Class1.cs
+++ FluentAAS/FluentAAS.Core/Class1.cs
@@ -2,4 +2,12 @@
 
 public class Class1
 {
+    public void init()
+    {
+        var element = new AasCore.Aas3_0.Property(AasCore.Aas3_0.DataTypeDefXsd.Int)
+                      {
+                          IdShort = "someElement",
+                          Value   = "1984"
+                      };
+    }
 }
