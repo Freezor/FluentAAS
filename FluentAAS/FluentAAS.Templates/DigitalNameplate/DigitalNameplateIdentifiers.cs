141fc0a4c213933a3bb2536a699f2b1aced3792c
Oliver <info@oliver-fries.de>
2025-12-08 09:21:44 +0100

    #2 FEAT reduce complexity in DigitalNameplateBuilder.cs

---
+++ FluentAAS/FluentAAS.Templates/DigitalNameplate/DigitalNameplateIdentifiers.cs
@@ -0,0 +1,25 @@
+namespace FluentAAS.Templates.DigitalNameplate;
+
+public static class DigitalNameplateIdentifiers
+{
+    public const string ManufacturerNameIdShort                   = "ManufacturerName";
+    public const string ManufacturerProductDesignationIdShort     = "ManufacturerProductDesignation";
+    public const string SerialNumberIdShort                       = "SerialNumber";
+    public const string UriOfTheProductIdShort                    = "UriOfTheProduct";
+    public const string ManufacturerProductRootIdShort            = "ManufacturerProductRoot";
+    public const string ManufacturerProductFamilyIdShort          = "ManufacturerProductFamily";
+    public const string ManufacturerProductTypeIdShort            = "ManufacturerProductType";
+    public const string OrderCodeOfManufacturerIdShort            = "OrderCodeOfManufacturer";
+    public const string ProductArticleNumberOfManufacturerIdShort = "ProductArticleNumberOfManufacturer";
+    public const string YearOfConstructionIdShort                 = "YearOfConstruction";
+    public const string DateOfManufactureIdShort                  = "DateOfManufacture";
+    public const string HardwareVersionIdShort                    = "HardwareVersion";
+    public const string FirmwareVersionIdShort                    = "FirmwareVersion";
+    public const string SoftwareVersionIdShort                    = "SoftwareVersion";
+    public const string CountryOfOriginIdShort                    = "CountryOfOrigin";
+    public const string UniqueFacilityIdentifierIdShort           = "UniqueFacilityIdentifier";
+    public const string AddressInformationIdShort                 = "AddressInformation";
+    public const string CompanyLogoIdShort                        = "CompanyLogo";
+    public const string MarkingsIdShort                           = "Markings";
+    public const string AssetSpecificPropertiesIdShort            = "AssetSpecificProperties";
+}
