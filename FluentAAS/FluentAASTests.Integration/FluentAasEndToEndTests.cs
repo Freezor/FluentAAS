fa95a87e4fd95fc65fae5288c4e9ff51976e2391
Oliver <info@oliver-fries.de>
2026-03-28 22:42:39 +0100

    Move TechnicalData composition example into integration tests

--- FluentAAS/FluentAASTests.Integration/FluentAasEndToEndTests.cs
+++ FluentAAS/FluentAASTests.Integration/FluentAasEndToEndTests.cs
@@ -163,22 +163,7 @@
         [Fact]
         public void CanCreateTechnicalDataWithFluentApi()
         {
-            var environment = AasBuilder.Create()
-                                        .AddShell("urn:aas:example:drivetrain:1000", "DriveTrainAAS")
-                                        .AddTechnicalData("urn:aas:submodel:technical-data:drivetrain:1000")
-                                        .WithMotorPerformance(motor => motor
-                                            .WithRatedVoltage(400m, "V", TechnicalDataSemantics.RatedVoltage)
-                                            .WithRatedCurrent(82.5m, "A", TechnicalDataSemantics.RatedCurrent)
-                                            .WithRatedPower(45m, "kW", TechnicalDataSemantics.RatedPower)
-                                            .WithRatedSpeed(1470m, "1/min", TechnicalDataSemantics.RatedSpeed))
-                                        .WithBearingCharacteristics(bearing => bearing
-                                            .WithInnerDiameter(50m, "mm", TechnicalDataSemantics.InnerDiameter)
-                                            .WithOuterDiameter(90m, "mm", TechnicalDataSemantics.OuterDiameter)
-                                            .WithWidth(20m, "mm", TechnicalDataSemantics.Width)
-                                            .WithLimitingSpeed(6500m, "1/min", TechnicalDataSemantics.LimitingSpeed))
-                                        .BuildTechnicalData()
-                                        .CompleteShellConfiguration()
-                                        .Build();
+            var environment = TechnicalDataCompositionExample.BuildMotorAndBearingExampleEnvironment();
 
             environment.AssetAdministrationShells.ShouldNotBeNull();
             environment.AssetAdministrationShells!.Count.ShouldBe(1);
